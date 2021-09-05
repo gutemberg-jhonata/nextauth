20bff1c41ba7d60ad0d9da241c05df25968d52d5
Jhonata <gutemberg.jhonata@gmail.com>
2021-09-05 12:43:31 -0300

    Remove unused imports in metrics page

--- pages/metrics.tsx
+++ pages/metrics.tsx
@@ -1,12 +1,6 @@
-import { useContext } from "react"
-import { Can } from "../components/Can"
-import { AuthContext } from "../contexts/AuthContext"
-import { setupAPIClient } from "../services/api"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Metrics() {
-  const { user } = useContext(AuthContext)
-
   return (
     <>
       <h1>Metrics</h1>
