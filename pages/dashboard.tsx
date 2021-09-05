38136adbc2656a41477e5fe2a065b5a44ff90448
Jhonata <gutemberg.jhonata@gmail.com>
2021-09-05 11:59:31 -0300

    Create useCan hook

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,14 +1,23 @@
-import { destroyCookie } from "nookies"
 import { useContext } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
 
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list'],
+    roles: ['administrator', 'editor']
+  })
+
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
   )
 }
 
